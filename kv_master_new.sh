43a28daf55ce27f25450e9d76df93eb7ad48aa61
Artur Henrique  Scheiner <artur.scheiner@gmail.com>
2023-10-27 15:25:11 -0300

    new adjustments

--- kv_master_new.sh
+++ kv_master_new.sh
@@ -48,57 +48,57 @@ modprobe br_netfilter
 echo '1' > /proc/sys/net/ipv4/ip_forward
 
 ### containerd
-# cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
-# overlay
-# br_netfilter
-# EOF
-# sudo modprobe overlay
-# sudo modprobe br_netfilter
-# cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
-# net.bridge.bridge-nf-call-iptables  = 1
-# net.ipv4.ip_forward                 = 1
-# net.bridge.bridge-nf-call-ip6tables = 1
-# EOF
-# sudo sysctl --system
-# sudo mkdir -p /etc/containerd
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+sudo modprobe overlay
+sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sudo sysctl --system
+sudo mkdir -p /etc/containerd
 
 
 ### containerd config
-# cat > /etc/containerd/config.toml <<EOF
-# disabled_plugins = []
-# imports = []
-# oom_score = 0
-# plugin_dir = ""
-# required_plugins = []
-# root = "/var/lib/containerd"
-# state = "/run/containerd"
-# version = 2
+cat > /etc/containerd/config.toml <<EOF
+disabled_plugins = []
+imports = []
+oom_score = 0
+plugin_dir = ""
+required_plugins = []
+root = "/var/lib/containerd"
+state = "/run/containerd"
+version = 2
 
-# [plugins]
+[plugins]
 
-#   [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
-#     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-#       base_runtime_spec = ""
-#       container_annotations = []
-#       pod_annotations = []
-#       privileged_without_host_devices = false
-#       runtime_engine = ""
-#       runtime_root = ""
-#       runtime_type = "io.containerd.runc.v2"
+  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+      base_runtime_spec = ""
+      container_annotations = []
+      pod_annotations = []
+      privileged_without_host_devices = false
+      runtime_engine = ""
+      runtime_root = ""
+      runtime_type = "io.containerd.runc.v2"
 
-#       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-#         BinaryName = ""
-#         CriuImagePath = ""
-#         CriuPath = ""
-#         CriuWorkPath = ""
-#         IoGid = 0
-#         IoUid = 0
-#         NoNewKeyring = false
-#         NoPivotRoot = false
-#         Root = ""
-#         ShimCgroup = ""
-#         SystemdCgroup = true
-# EOF
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+        BinaryName = ""
+        CriuImagePath = ""
+        CriuPath = ""
+        CriuWorkPath = ""
+        IoGid = 0
+        IoUid = 0
+        NoNewKeyring = false
+        NoPivotRoot = false
+        Root = ""
+        ShimCgroup = ""
+        SystemdCgroup = true
+EOF
 
 
 # ### crictl uses containerd as default
