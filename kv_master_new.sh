526805e2d0ed0f5ee21d01dea6e367d084ba33a8
Artur Henrique  Scheiner <artur.scheiner@gmail.com>
2023-10-27 15:31:38 -0300

    adjustments

--- kv_master_new.sh
+++ kv_master_new.sh
@@ -64,41 +64,41 @@ sudo mkdir -p /etc/containerd
 
 
 ### containerd config
-cat > /etc/containerd/config.toml <<EOF
-disabled_plugins = []
-imports = []
-oom_score = 0
-plugin_dir = ""
-required_plugins = []
-root = "/var/lib/containerd"
-state = "/run/containerd"
-version = 2
+# cat > /etc/containerd/config.toml <<EOF
+# disabled_plugins = []
+# imports = []
+# oom_score = 0
+# plugin_dir = ""
+# required_plugins = []
+# root = "/var/lib/containerd"
+# state = "/run/containerd"
+# version = 2
 
-[plugins]
+# [plugins]
 
-  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
-    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-      base_runtime_spec = ""
-      container_annotations = []
-      pod_annotations = []
-      privileged_without_host_devices = false
-      runtime_engine = ""
-      runtime_root = ""
-      runtime_type = "io.containerd.runc.v2"
+#   [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+#     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+#       base_runtime_spec = ""
+#       container_annotations = []
+#       pod_annotations = []
+#       privileged_without_host_devices = false
+#       runtime_engine = ""
+#       runtime_root = ""
+#       runtime_type = "io.containerd.runc.v2"
 
-      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-        BinaryName = ""
-        CriuImagePath = ""
-        CriuPath = ""
-        CriuWorkPath = ""
-        IoGid = 0
-        IoUid = 0
-        NoNewKeyring = false
-        NoPivotRoot = false
-        Root = ""
-        ShimCgroup = ""
-        SystemdCgroup = true
-EOF
+#       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+#         BinaryName = ""
+#         CriuImagePath = ""
+#         CriuPath = ""
+#         CriuWorkPath = ""
+#         IoGid = 0
+#         IoUid = 0
+#         NoNewKeyring = false
+#         NoPivotRoot = false
+#         Root = ""
+#         ShimCgroup = ""
+#         SystemdCgroup = true
+# EOF
 
 
 # ### crictl uses containerd as default
