e4b69329711dee45b80114ea774bfc519af7c80c
Artur Henrique  Scheiner <artur.scheiner@gmail.com>
2023-10-28 07:57:07 -0300

    changes

--- kv_master_new.sh
+++ kv_master_new.sh
@@ -64,57 +64,57 @@ echo '1' > /proc/sys/net/ipv4/ip_forward
 
 
 ### containerd config
-# cat > /etc/containerd/config.toml <<EOF
-# disabled_plugins = []
-# imports = []
-# oom_score = 0
-# plugin_dir = ""
-# required_plugins = []
-# root = "/var/lib/containerd"
-# state = "/run/containerd"
-# version = 2
+cat > /etc/containerd/config.toml <<EOF
+disabled_plugins = []
+imports = []
+oom_score = 0
+plugin_dir = ""
+required_plugins = []
+root = "/var/lib/containerd"
+state = "/run/containerd"
+version = 2
 
-# [plugins]
+[plugins]
 
-#   [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
-#     [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-#       base_runtime_spec = ""
-#       container_annotations = []
-#       pod_annotations = []
-#       privileged_without_host_devices = false
-#       runtime_engine = ""
-#       runtime_root = ""
-#       runtime_type = "io.containerd.runc.v2"
+  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+      base_runtime_spec = ""
+      container_annotations = []
+      pod_annotations = []
+      privileged_without_host_devices = false
+      runtime_engine = ""
+      runtime_root = ""
+      runtime_type = "io.containerd.runc.v2"
 
-#       [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-#         BinaryName = ""
-#         CriuImagePath = ""
-#         CriuPath = ""
-#         CriuWorkPath = ""
-#         IoGid = 0
-#         IoUid = 0
-#         NoNewKeyring = false
-#         NoPivotRoot = false
-#         Root = ""
-#         ShimCgroup = ""
-#         SystemdCgroup = true
-# EOF
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+        BinaryName = ""
+        CriuImagePath = ""
+        CriuPath = ""
+        CriuWorkPath = ""
+        IoGid = 0
+        IoUid = 0
+        NoNewKeyring = false
+        NoPivotRoot = false
+        Root = ""
+        ShimCgroup = ""
+        SystemdCgroup = true
+EOF
 
 
 # ### crictl uses containerd as default
-{
-cat <<EOF | sudo tee /etc/crictl.yaml
-runtime-endpoint: unix:///run/containerd/containerd.sock
-EOF
-}
+# {
+# cat <<EOF | sudo tee /etc/crictl.yaml
+# runtime-endpoint: unix:///run/containerd/containerd.sock
+# EOF
+# }
 
 
-# ### kubelet should use containerd
-{
-cat <<EOF | sudo tee /etc/default/kubelet
-KUBELET_EXTRA_ARGS="--container-runtime remote --container-runtime-endpoint unix:///run/containerd/containerd.sock"
-EOF
-}
+# # ### kubelet should use containerd
+# {
+# cat <<EOF | sudo tee /etc/default/kubelet
+# KUBELET_EXTRA_ARGS="--container-runtime remote --container-runtime-endpoint unix:///run/containerd/containerd.sock"
+# EOF
+# }
 
 
 ### install podman
