9e796b8ffc52b8158587c679b2988b13d6bbd824
Artur Henrique  Scheiner <artur.scheiner@gmail.com>
2023-10-31 15:13:06 -0300

    Adding a bootstrap.sh script

---
+++ bootstrap.sh
@@ -0,0 +1,193 @@
+#!/bin/sh
+
+# Source: http://kubernetes.io/docs/getting-started-guides/kubeadm
+#
+#set -e
+
+#KUBE_VERSION=1.23.4
+
+
+### setup terminal
+#apt-get update
+#apt-get install -y bash-completion binutils
+# echo 'colorscheme ron' >> ~/.vimrc
+# echo 'set tabstop=2' >> ~/.vimrc
+# echo 'set shiftwidth=2' >> ~/.vimrc
+# echo 'set expandtab' >> ~/.vimrc
+# echo 'source <(kubectl completion bash)' >> ~/.bashrc
+# echo 'alias k=kubectl' >> ~/.bashrc
+# echo 'alias c=clear' >> ~/.bashrc
+# echo 'complete -F __start_kubectl k' >> ~/.bashrc
+# sed -i '1s/^/force_color_prompt=yes\n/' ~/.bashrc
+
+# source ~/.bashrc
+
+### disable linux swap and remove any existing swap partitions
+# swapoff -a
+# sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+
+### remove packages
+# kubeadm reset -f || true
+# crictl rm --force $(crictl ps -a -q) || true
+# apt-mark unhold kubelet kubeadm kubectl kubernetes-cni || true
+# apt-get remove -y docker.io containerd kubelet kubeadm kubectl kubernetes-cni || true
+# apt-get autoremove -y
+# systemctl daemon-reload
+
+
+### install packages
+# apt-transport-https may be a dummy package; if so, you can skip that package
+# apt-get install -y apt-transport-https ca-certificates curl gpg
+# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y containerd kubelet kubeadm kubectl kubernetes-cni
+apt-mark hold kubelet kubeadm kubectl kubernetes-cni
+
+# modprobe br_netfilter
+# echo '1' > /proc/sys/net/ipv4/ip_forward
+
+### containerd
+cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
+overlay
+br_netfilter
+EOF
+# sudo modprobe overlay
+# sudo modprobe br_netfilter
+cat <<EOF | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.ipv4.ip_forward                 = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+sysctl --system
+# mkdir -p /etc/containerd
+
+
+### containerd config
+cat > /etc/containerd/config.toml <<EOF
+disabled_plugins = []
+imports = []
+oom_score = 0
+plugin_dir = ""
+required_plugins = []
+root = "/var/lib/containerd"
+state = "/run/containerd"
+version = 2
+
+[plugins]
+
+  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes]
+    [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+      base_runtime_spec = ""
+      container_annotations = []
+      pod_annotations = []
+      privileged_without_host_devices = false
+      runtime_engine = ""
+      runtime_root = ""
+      runtime_type = "io.containerd.runc.v2"
+
+      [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+        BinaryName = ""
+        CriuImagePath = ""
+        CriuPath = ""
+        CriuWorkPath = ""
+        IoGid = 0
+        IoUid = 0
+        NoNewKeyring = false
+        NoPivotRoot = false
+        Root = ""
+        ShimCgroup = ""
+        SystemdCgroup = true
+EOF
+
+
+# ### crictl uses containerd as default
+{
+cat <<EOF | sudo tee /etc/crictl.yaml
+runtime-endpoint: unix:///run/containerd/containerd.sock
+EOF
+}
+
+
+# # ### kubelet should use containerd
+# {
+# cat <<EOF | sudo tee /etc/default/kubelet
+# KUBELET_EXTRA_ARGS="--container-runtime remote --container-runtime-endpoint unix:///run/containerd/containerd.sock"
+# EOF
+# }
+
+
+### install podman
+# apt-get install software-properties-common -y
+# add-apt-repository -y ppa:projectatomic/ppa
+# sudo apt-get -qq -y install podman containers-common
+# ### install podman
+# cat <<EOF | sudo tee /etc/containers/registries.conf
+# [registries.search]
+# registries = ['docker.io']
+# EOF
+
+# if [[ $(podman network ls | grep podman) ]]; then podman network rm podman; fi
+
+
+# ### start services
+# systemctl daemon-reload
+# systemctl enable containerd
+# systemctl restart containerd
+# systemctl enable kubelet && systemctl start kubelet
+
+
+# ### init k8s
+# rm /root/.kube/config || true
+# kubeadm init --ignore-preflight-errors=NumCPU --skip-token-print
+
+# mkdir -p ~/.kube
+# sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+
+# Weave Net can be installed onto your CNI-enabled Kubernetes cluster with a single command
+#kubectl apply -f https://github.com/weaveworks/weave/releases/download/v2.8.1/weave-daemonset-k8s.yaml
+
+# workaround because https://github.com/weaveworks/weave/issues/3927
+# kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
+# curl -L https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n') -o weave.yaml
+# sed -i 's/ghcr.io\/weaveworks\/launcher/docker.io\/weaveworks/g' weave.yaml
+# kubectl -f weave.yaml apply
+# rm weave.yaml
+
+# apt-mark unhold kubelet kubeadm kubectl kubernetes-cni
+
+# curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.3/manifests/calico.yaml -O
+# kubectl apply -f calico.yaml
+
+# etcdctl
+# ETCD_VER=v3.5.10
+
+# # choose either URL
+# GOOGLE_URL=https://storage.googleapis.com/etcd
+# GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
+# DOWNLOAD_URL=${GOOGLE_URL}
+# DOWNLOAD_PATH='/tmp'
+# EXTRACT_PATH='/tmp/etcd-download-test'
+# MACHINE="$(uname -m)"
+
+# if [[ $MACHINE == "x86_64" || $MACHINE == "amd64" ]]; then
+#     rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+#     rm -rf /tmp/etcd-download-test && mkdir -p /tmp/etcd-download-test
+
+#     curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o ${DOWNLOAD_PATH}/etcd-${ETCD_VER}-linux-amd64.tar.gz
+#     tar xzvf ${DOWNLOAD_PATH}/etcd-${ETCD_VER}-linux-amd64.tar.gz -C ${DOWNLOAD_PATH}/etcd-download-test --strip-components=1
+#     rm -f ${DOWNLOAD_PATH}/etcd-${ETCD_VER}-linux-amd64.tar.gz
+
+#     ${EXTRACT_PATH}/etcd --version
+#     ${EXTRACT_PATH}/etcdctl version
+#     ${EXTRACT_PATH}/etcdutl version
+#     mv ${EXTRACT_PATH}/* /usr/bin/
+#     # rm -rf ${ETCDCTL_VERSION_FULL} ${ETCDCTL_VERSION_FULL}.tar.gz
+# else
+#     snap install etcd
+# fi
+
+# echo
+# echo "### COMMAND TO ADD A WORKER NODE ###"
+# kubeadm token create --print-join-command --ttl 0
